609fbd97342cd407793d4963e77457d4c409a96f
Sreejith <probubble01@gmail.com>
2023-03-05 17:13:20 +0530

    heheheh

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, send_from_directory
 import json
+import mat
+from pathlib import Path
+
+root = Path(__file__).parent
+UPLOAD_FOLDER = root
 app = Flask(__name__)
 
 @app.route("/", methods = ('GET','POST'))
@@ -8,5 +13,10 @@ def receive():
         data = request.form['jsonData']
         with open('data.json','w') as f1:
             f1.write(data)
-        return render_template('result.html', data = data)
+            mat.plotit()
+        return render_template('result.html', filename = "graph.png")
     return render_template('index.html')
+
+@app.route('/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
--- mat.py
+++ mat.py
@@ -1,22 +1,21 @@
 import json
 import matplotlib.pyplot as plt
-crimes = {}
-d = {}
-ls = []
-with open("data.json","r") as f:
-    data = json.load(f)
-    for i in data:
-        jsonData = []
-        for j in i['Data']:
-            d = i['Data'].get("Totals")
-        if crimes == {} or i.get("State","") not in crimes:
-            crimes[i['State']] = d['Property'].get("All")
-print(crimes)
 
+def plotit():
+    crimes = {}
+    d = {}
+    ls = []
+    with open("data.json","r") as f:
+        data = json.load(f)
+        for i in data:
+            jsonData = []
+            for j in i['Data']:
+                d = i['Data'].get("Totals")
+            if crimes == {} or i.get("State","") not in crimes:
+                crimes[i['State']] = d['Property'].get("All")
 
-state = list(crimes.keys())
-total = list(crimes.values())
-
-plt.bar(range(len(crimes)), total, tick_label=state)
-plt.xticks(rotation=90, ha='right')
-plt.show()
+    state = list(crimes.keys())
+    total = list(crimes.values())
+    plt.bar(range(len(crimes)), total, tick_label=state)
+    plt.xticks(rotation=-45, ha='right')
+    plt.savefig('graph.png')
