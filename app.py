da412268f47e183d1cde245abb083b77961ab041
Sreejith <probubble01@gmail.com>
2023-03-05 15:16:59 +0530

    okkaaaa

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, url_for, redirect, request
 import json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ('GET', 'POST'))
 def receive():
     data = request.form['jsonData']
     if request.method == 'POST':
         data = json.load(request.form['jsonData'])
-
-    return render_template('result.html', data = data)
+        return render_template('result.html', data = data)
+    return render_template('index.html')
 
 
