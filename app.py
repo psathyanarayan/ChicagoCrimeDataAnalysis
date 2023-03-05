d679ba75194afe727841d04b067fa3e7ab5b9838
P Sathya Narayan <sathyanmbpk@gmail.com>
2023-03-05 15:06:57 +0530

    CSS v2

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def receive():
+    data = request.form['jsonData']
     if request.method == 'POST':
         data = json.load(request.form['jsonData'])
 
-    return "<p>Hello, World!</p>"
+
 
