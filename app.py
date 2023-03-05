bc4a1f9b3f448deb667b3d6dc5fc543225e2194d
Sreejith <probubble01@gmail.com>
2023-03-05 15:41:13 +0530

    fix json

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, url_for, redirect, request
 import json
 app = Flask(__name__)
 
-@app.route("/", methods = ('GET', 'POST'))
+@app.route("/", methods = ('GET','POST'))
 def receive():
-    data = request.form['jsonData']
+    #data = request.form['jsonData']
     if request.method == 'POST':
         data = json.load(request.form['jsonData'])
-        return render_template('result.html', data = data)
+        #return render_template('result.html', data = data)
     return render_template('index.html')
-
-
